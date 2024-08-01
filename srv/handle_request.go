d0146abf08931742919806d262a5f7bb760f1a77
John Gustafson <vatlnetwork@gmail.com>
2024-08-01 11:05:50 -0600

    write options request handler

--- srv/handle_request.go
+++ srv/handle_request.go
@@ -10,4 +10,9 @@ func HandleRequest(route routes.Route) http.HandlerFunc {
 	}
 }
 
-func HandleOptions(rw http.ResponseWriter, req *http.Request) {}
+func HandleOptions(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	rw.Header().Set("Access-Control-Allow-Methods", "*")
+	rw.Header().Set("Access-Control-Allow-Headers", "*")
+	rw.WriteHeader(http.StatusOK)
+}
