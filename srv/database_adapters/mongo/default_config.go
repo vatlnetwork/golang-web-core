f0e4f56f698d0a5da920ed6f9e87f0961c7ec2f6
John Gustafson <vatlnetwork@gmail.com>
2025-01-30 16:33:52 -0700

    changed default mongo database name

--- srv/database_adapters/mongo/default_config.go
+++ srv/database_adapters/mongo/default_config.go
@@ -5,6 +5,6 @@ import databaseadapters "golang-web-core/srv/database_adapters"
 func DefaultConfig() databaseadapters.ConnectionConfig {
 	return databaseadapters.ConnectionConfig{
 		Hostname: "localhost:27017",
-		Database: "vatlnetworkWeb",
+		Database: "vatlnetwork",
 	}
 }
