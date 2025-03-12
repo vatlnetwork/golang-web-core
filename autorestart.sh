e819101d989f4c5aaa260e8c15d851354185491b
John Gustafson <vatlnetwork@gmail.com>
2025-03-12 16:56:20 -0600

    added autorestart system

---
+++ autorestart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this requires inotify-tools and a linux system in order to run
+while true; do
+  go build
+  $@ &
+  PID=$!
+  echo "PID: $PID"
+  inotifywait -r -e modify .
+  kill $PID
+done
