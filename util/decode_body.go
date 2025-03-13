9ae254b4609947c45b98094094c00ab6c78eb51d
John Gustafson <vatlnetwork@gmail.com>
2025-03-13 11:38:28 -0600

    add decode request body to map functionality

--- util/decode_body.go
+++ util/decode_body.go
@@ -19,3 +19,18 @@ func DecodeRequestBody(req *http.Request, decodeObject interface{}) error {
 
 	return nil
 }
+
+func DecodeRequestBodyToMap(req *http.Request) (map[string]any, error) {
+	bytes, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var decoded map[string]any
+	err = json.Unmarshal(bytes, &decoded)
+	if err != nil {
+		return nil, err
+	}
+
+	return decoded, nil
+}
