716fb5b17cb0da19caa0b3a768442ae1292a94d3
John Gustafson <vatlnetwork@gmail.com>
2025-03-13 16:06:04 -0600

    add form data decoder

--- util/decode_body.go
+++ util/decode_body.go
@@ -34,3 +34,26 @@ func DecodeRequestBodyToMap(req *http.Request) (map[string]any, error) {
 
 	return decoded, nil
 }
+
+func DecodeFormDataToMap(req *http.Request, maxSize ...int64) (map[string]any, error) {
+	size := int64(100)
+	if len(maxSize) > 0 {
+		size = maxSize[0]
+	}
+
+	err := req.ParseMultipartForm(size << 20)
+	if err != nil {
+		return nil, err
+	}
+
+	decoded := map[string]any{}
+	for key, value := range req.MultipartForm.Value {
+		decoded[key] = value[0]
+	}
+
+	for key, value := range req.MultipartForm.File {
+		decoded[key] = value[0]
+	}
+
+	return decoded, nil
+}
