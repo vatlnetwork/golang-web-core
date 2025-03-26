201cc677854a8c0ca5704863e4d9ddb203ea2b76
John Gustafson <vatlnetwork@gmail.com>
2025-03-26 09:50:49 -0600

    added 401 error handler

--- srv/srverr/handle_error.go
+++ srv/srverr/handle_error.go
@@ -5,19 +5,24 @@ import (
 	"net/http"
 )
 
-func Handle500(rw http.ResponseWriter, err error) {
-	http.Error(rw, err.Error(), http.StatusInternalServerError)
-	util.LogColor("red", "INTERNAL SERVER ERROR: %v", err.Error())
-}
-
 func Handle400(rw http.ResponseWriter, err error) {
 	http.Error(rw, err.Error(), http.StatusBadRequest)
 	util.LogColor("yellow", "BAD REQUEST: %v", err.Error())
 }
 
+func Handle401(rw http.ResponseWriter, err error) {
+	http.Error(rw, err.Error(), http.StatusUnauthorized)
+	util.LogColor("yellow", "UNAUTHORIZED: %v", err.Error())
+}
+
 func Handle404(rw http.ResponseWriter, err error) {
 	http.Error(rw, err.Error(), http.StatusNotFound)
 	util.LogColor("yellow", "NOT FOUND: %v", err.Error())
+}
+
+func Handle500(rw http.ResponseWriter, err error) {
+	http.Error(rw, err.Error(), http.StatusInternalServerError)
+	util.LogColor("red", "INTERNAL SERVER ERROR: %v", err.Error())
 }
 
 func HandleError(code int, rw http.ResponseWriter, err error) {
