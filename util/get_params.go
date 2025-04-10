d06758373a406103660c337c26cde97aac0ade15
John Gustafson <vatlnetwork@gmail.com>
2025-04-10 09:00:40 -0600

    added max upload size option to util.GetParams

--- util/get_params.go
+++ util/get_params.go
@@ -4,7 +4,12 @@ import (
 	"net/http"
 )
 
-func GetParams(req *http.Request) (map[string]any, error) {
+func GetParams(req *http.Request, maxSize ...int64) (map[string]any, error) {
+	size := int64(100)
+	if len(maxSize) > 0 {
+		size = maxSize[0]
+	}
+
 	if req.Method == http.MethodGet {
 		queryValues := req.URL.Query()
 		params := make(map[string]any)
@@ -16,7 +21,7 @@ func GetParams(req *http.Request) (map[string]any, error) {
 
 	params, err := DecodeRequestBodyToMap(req)
 	if err != nil {
-		params, err = DecodeFormDataToMap(req)
+		params, err = DecodeFormDataToMap(req, size)
 		if err != nil {
 			return nil, err
 		}
